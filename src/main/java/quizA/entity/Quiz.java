b589f177ebc013e7525de64666dcfcd4a1eb9671
3030670 <3030670@30306-70>
2022-07-08 09:38:15 +0900

    内容追加

--- src/main/java/quizA/entity/Quiz.java
+++ src/main/java/quizA/entity/Quiz.java
@@ -1,5 +1,25 @@
 package quizA.entity;
 
-public class Quiz {
+import org.springframework.data.annotation.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+	/** 識別ID */
+	@Id
+	private Integer id;
+	
+	/** クイズの内容 */
+	private String question;
+	
+	/** クイズの解答 */
+	private Boolean answer;
+	
+	/** 作成者 */
+	private String author;
 }
