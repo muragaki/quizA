eee481276c5d48277972646d4f781ab8d12c847a
3030670 <3030670@30306-70>
2022-07-08 11:45:59 +0900

    Revert "内容追加"
    This reverts commit b589f177ebc013e7525de64666dcfcd4a1eb9671.

--- src/main/java/quizA/repository/QuizRepository.java
+++ src/main/java/quizA/repository/QuizRepository.java
@@ -1,11 +1,5 @@
 package quizA.repository;
 
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+public class QuizRepository {
 
-import quizA.entity.Quiz;
-
-public interface QuizRepository extends CrudRepository<Quiz, Integer>{
-	@Query("SERECT id FROM quiz ORDER BY RANDOM() limit 1")
-	Integer getRandomId();
 }
